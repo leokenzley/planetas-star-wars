21676dccee62c4f05b73673c8c952fb889e03d12
Leo Kenzley <leo.oliveira@lifecon.com.br>
2021-02-18 08:31:39 -0300

    Commit inicial

---
+++ src/main/kotlin/com/starwars/starwars/PlanetasDeStarWarsApplication.kt
@@ -0,0 +1,11 @@
+package com.starwars.starwars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanetasDeStarWarsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanetasDeStarWarsApplication>(*args)
+}
